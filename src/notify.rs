8526b17ed92ecccf1b8b6fa3e34890b54a7153bf
Daniel (dmilith) Dettlaff <dmilith@me.com>
2023-05-11 21:37:33 +0200

    new: Refactored notify module to be fully async. Defined notify::notify_success_with_retry(). Moved consts to the standalone module

--- src/notify.rs
+++ src/notify.rs
@@ -1,84 +1,88 @@
 use crate::*;
 
-use retry::{delay::Fixed, retry_with_index, OperationResult};
-use slack_hooked::{AttachmentBuilder, PayloadBuilder, Slack};
-
-/// Default Notification name:
-const DEFAULT_SLACK_NAME: &str = "CertsD";
-
-/// Default Notification failure icon:
-const DEFAULT_SLACK_FAILURE_ICON: &str = ":error:";
-
-/// Default Notification success icon:
-const DEFAULT_SLACK_SUCCESS_ICON: &str = ":white_check_mark:";
-
-/// Default failure notification color:
-const DEFAULT_SLACK_FAILURE_COLOR: &str = "#ff1111";
-
-/// Default success notification color:
-const DEFAULT_SLACK_SUCCESS_COLOR: &str = "#00ff00";
+use slack_hook2::{AttachmentBuilder, PayloadBuilder, Slack, SlackError};
+use tokio::time::Duration;
+use try_again::{retry_async, Delay, Retry, TokioSleep};
 
 
 /// Sends success notification to Slack
 #[instrument]
-pub fn notify_success(webhook: &str, message: &str) {
+pub async fn notify_success(webhook: &str, message: &str) -> Result<(), SlackError> {
     let success_emoji = String::from(DEFAULT_SLACK_SUCCESS_ICON);
-    notify(webhook, message, &success_emoji, false)
+    notify(webhook, message, &success_emoji, false).await
 }
 
 
 /// Sends failure notification to Slack
 #[instrument]
-pub fn notify_failure(webhook: &str, message: &str) {
+pub async fn notify_failure(webhook: &str, message: &str) -> Result<(), SlackError> {
     let failure_emoji = String::from(DEFAULT_SLACK_FAILURE_ICON);
-    notify(webhook, message, &failure_emoji, true)
+    notify(webhook, message, &failure_emoji, true).await
+}
+
+
+/// Send notification to Slack with retry on failure
+#[instrument]
+pub async fn notify_success_with_retry(
+    config: &Config,
+    domain: &str,
+    wildcard: bool,
+) -> Result<(), SlackError> {
+    // send success notification using a Slack webhook
+    retry_async(
+        Retry {
+            max_tries: DEFAULT_MAX_NOTIFICATION_RETRIES,
+            delay: Some(Delay::Static {
+                delay: Duration::from_millis(DEFAULT_NOTIFICATION_RETRT_PAUSE_MS),
+            }),
+        },
+        TokioSleep {},
+        move || {
+            async move {
+                let slack_webhook = &config.slack_webhook().await;
+                let message = if wildcard {
+                    format!("Certificate renewal succeeded for the domain: *.{domain}.")
+                } else {
+                    format!("Certificate renewal succeeded for the domain: {domain}.")
+                };
+                notify_success(&slack_webhook.to_owned(), &message).await
+            }
+        },
+    )
+    .await
 }
 
 
 /// Sends generic notification over Slack
 #[instrument]
-fn notify(webhook: &str, message: &str, icon: &str, fail: bool) {
+async fn notify(
+    webhook: &str,
+    message: &str,
+    icon: &str,
+    fail: bool,
+) -> Result<(), SlackError> {
     if webhook.is_empty() {
         warn!("Webhook undefined. Notifications will not be sent.");
-        return;
+        return Ok(());
     }
-    retry_with_index(Fixed::from_millis(1000), |current_try| {
-        if current_try > 3 {
-            return OperationResult::Err("Did not succeed within 3 tries");
-        }
+    let slack = Slack::new(webhook)?;
+    let payload = PayloadBuilder::new()
+        .username(DEFAULT_SLACK_NAME)
+        .icon_emoji(icon)
+        .attachments(vec![
+            if fail {
+                AttachmentBuilder::new(message)
+                    .color(DEFAULT_SLACK_FAILURE_COLOR)
+                    .build()
+                    .unwrap_or_default()
+            } else {
+                AttachmentBuilder::new(message)
+                    .color(DEFAULT_SLACK_SUCCESS_COLOR)
+                    .build()
+                    .unwrap_or_default()
+            },
+        ])
+        .build()?;
 
-        let notification = Slack::new(webhook).and_then(|slack| {
-            PayloadBuilder::new()
-                .username(DEFAULT_SLACK_NAME)
-                .icon_emoji(icon)
-                .attachments(vec![
-                    if fail {
-                        AttachmentBuilder::new(message)
-                            .color(DEFAULT_SLACK_FAILURE_COLOR)
-                            .build()
-                            .unwrap_or_default()
-                    } else {
-                        AttachmentBuilder::new(message)
-                            .color(DEFAULT_SLACK_SUCCESS_COLOR)
-                            .build()
-                            .unwrap_or_default()
-                    },
-                ])
-                .build()
-                .and_then(|payload| {
-                    debug!("Sending notification with payload: {payload:?}");
-                    slack.send(&payload)
-                })
-        });
-
-        match notification {
-            Ok(_) => OperationResult::Ok("Sent!"),
-            Err(_) => OperationResult::Retry("Failed to send notification!"),
-        }
-    })
-    .map_err(|err| {
-        error!("Error sending notification: {err}");
-        err
-    })
-    .unwrap_or_default();
+    slack.send(&payload).await
 }
