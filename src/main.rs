3818f34bd508ff2d2fb3a3f6592ab0bb07f57b4e
Daniel (dmilith) Dettlaff <dmilith@me.com>
2023-05-13 20:33:15 +0200

    new: Moved initialize_logger to lib

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,5 @@
 use certsd::*;
 use hyperacme::Error;
-use tracing_subscriber::{
-    fmt::{
-        format::{Compact, DefaultFields, Format},
-        Layer, *,
-    },
-    layer::Layered,
-    reload::*,
-    EnvFilter, Registry,
-};
-
-
-type TracingEnvFilterHandle =
-    Handle<EnvFilter, Layered<Layer<Registry, DefaultFields, Format<Compact>>, Registry>>;
-
-
-#[instrument]
-fn initialize_logger() -> TracingEnvFilterHandle {
-    let env_log_filter = match EnvFilter::try_from_env("LOG") {
-        Ok(env_value_from_env) => env_value_from_env,
-        Err(_) => EnvFilter::from("info"),
-    };
-    let fmt = fmt()
-        .compact()
-        .with_target(true)
-        .with_line_number(false)
-        .with_file(false)
-        .with_thread_names(false)
-        .with_thread_ids(false)
-        .with_ansi(true)
-        .with_env_filter(env_log_filter)
-        .with_filter_reloading();
-
-    let handle = fmt.reload_handle();
-    fmt.init();
-    handle
-}
 
 
 #[instrument]
